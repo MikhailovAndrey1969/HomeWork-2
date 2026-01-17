c34e2d7f7df20548095641fadb78800899ae9e64
MikhailovAndrey1969 <mikhailov.andrey.d@gmail.com>
2026-01-17 17:09:40 +0500

    Add files via upload

---
+++ Source2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <Windows.h>
+
+int main() {
+
+	// setlocale(LC_CTYPE, "rus");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	std::string	str{};
+
+	std::cout << "Введите слово:" << std::endl;
+	std::getline(std::cin, str);
+	std::cout << "Вы ввели:" << std::endl;
+	std::cout << str << std::endl;
+
+	return 0;
+}
+ 
