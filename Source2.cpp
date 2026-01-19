5ee13e46b8bd710e560729545e5be6806f8620f1
MikhailovAndrey1969 <mikhailov.andrey.d@gmail.com>
2026-01-19 18:53:57 +0500

    Add files via upload

---
+++ Source2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+//#include <Windows.h>
+#include <clocale>
+
+int main() {
+
+	//setlocale(LC_CTYPE, "rus"); 
+	//SetConsoleCP(1251);
+	//SetConsoleOutputCP(1251); 
+	
+	setlocale(LC_ALL, "");	
+	std::wcin.imbue(std::locale("rus_rus.866"));
+	
+	std::wstring	str{};
+
+	std::wcout << L"Введите слово:" << std::endl;
+	std::getline(std::wcin, str);
+	std::wcout << L"Вы ввели:" << std::endl;
+	std::wcout << str << std::endl;
+		
+	return 0;
+}
+ 
